b83028ee423f01f824ed3ce39656460ce1504e83
MBenedicte <mbenedicte20@gmail.com>
2020-02-12 20:58:43 +0200

    Add a new post

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const express = require("express");
+const express = require('express');
 
-const app= express();
+const app = express();
 
-app.get("/", (req, res)=>{
+app.post('/post', (req, res) => {
+  const { name, phone } = req.body;
+  res.status(200).send({
+    name,
+    phone,
+    status: 200
+  });
+});
+app.get('/', (req, res) => {
   res.status(200).json({
-    message:"app is running"
-  })
-})
+    message: 'app is running'
+  });
+});
 
 const port = 4000;
 
-app.listen(port, ()=>console.log("app is running from port ", port));
+app.listen(port, () => console.log('app is running from port ', port));
